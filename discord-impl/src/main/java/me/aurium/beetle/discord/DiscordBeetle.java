6269f8c2d2c1acdb26102557d572fb75efb72344
Aurium <aukrais@gmail.com>
2021-03-19 17:54:49 -0700

    make build stop failing

--- discord-impl/src/main/java/me/aurium/beetle/discord/DiscordBeetle.java
+++ discord-impl/src/main/java/me/aurium/beetle/discord/DiscordBeetle.java
@@ -3,12 +3,11 @@ package me.aurium.beetle.discord;
 import me.aurium.beetle.defaults.CommonBeetle;
 import me.aurium.beetle.api.datacore.DataCoreFactory;
 import me.aurium.beetle.api.file.FileProvider;
-import me.aurium.beetle.api.logger.SimpleLogger;
 import me.aurium.beetle.api.service.ServiceRegistry;
 import me.aurium.beetle.api.task.Tasker;
 
 public class DiscordBeetle extends CommonBeetle {
-    public DiscordBeetle(Tasker tasker, SimpleLogger logger, DataCoreFactory factory, ServiceRegistry serviceRegistry, FileProvider fileProvider, boolean isDebug) {
-        super(tasker, logger, factory, serviceRegistry, fileProvider, isDebug);
+    public DiscordBeetle(Tasker tasker, DataCoreFactory factory, ServiceRegistry serviceRegistry, FileProvider fileProvider, boolean isDebug) {
+        super(tasker, factory, serviceRegistry, fileProvider, isDebug);
     }
 }
